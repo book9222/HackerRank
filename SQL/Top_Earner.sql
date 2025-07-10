48e3e5a3062c948959a96b058b5e43d4d5f67c61
Nattapat Boonwirotrit <nattapat.boonwirotrit@gmail.com>
2025-07-10 15:36:29 +0700

    add orther solution - not correct

--- SQL/Top_Earner.sql
+++ SQL/Top_Earner.sql
@@ -12,3 +12,15 @@ where
     ) = months * salary
 group by
     total_slr
+    -- select
+    --     max(total_slr) as max_total_slr,
+    --     count(total_slr) as c_total_slr
+    -- from
+    --     (
+    --         select
+    --             months * salary as total_slr
+    --         from
+    --             Employee
+    --     ) as total_slr_table
+    -- group by
+    --     total_slr
