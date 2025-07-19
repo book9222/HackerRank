7203f7c124fd956bdb442b0fb4ee1dd3294cb92a
Nattapat Boonwirotrit <nattapat.boonwirotrit@gmail.com>
2025-07-19 15:19:46 +0700

    numpy call on axis

---
+++ Basic-Python/Mean, Var, and Std.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+n, m = input().split()
+# print(n,m)
+
+mat = []
+for i in range(int(n)):
+    # mat.append(input().split())
+    mat.append([int(i) for i in input().split()])
+    
+np_array = np.array(mat)
+# print(np_array)
+print(np.mean(np_array, axis = 1))
+print(np.var(np_array, axis = 0))
+print(round(np.std(np_array),11))
