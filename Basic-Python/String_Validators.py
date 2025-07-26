b89c04b701be36df2ea069f15ead5c65e05183b5
Nattapat Boonwirotrit <nattapat.boonwirotrit@gmail.com>
2025-07-26 15:08:10 +0700

    rework string validation

--- Basic-Python/String_Validators.py
+++ Basic-Python/String_Validators.py
@@ -1,52 +1,54 @@
-if __name__ == '__main__':
-    s = input()
-    ans_f = False
-    for i in s:
-        if i.isalnum():
-            print("True")
-            ans_f = False
-            break
-        ans_f = True
-    if ans_f : 
-        print("False")
+# if __name__ == '__main__':
+#     s = input()
+#     ans_f = False
+#     for i in s:
+#         if i.isalnum():
+#             print("True")
+#             ans_f = False
+#             break
+#         ans_f = True
+#     if ans_f : 
+#         print("False")
     
-    ans_f = False
-    for i in s:
-        if i.isalpha():
-            print("True")
-            ans_f = False
-            break
-        ans_f = True
-    if ans_f : 
-        print("False")
+#     ans_f = False
+#     for i in s:
+#         if i.isalpha():
+#             print("True")
+#             ans_f = False
+#             break
+#         ans_f = True
+#     if ans_f : 
+#         print("False")
             
-    ans_f = False
-    for i in s:
-        if i.isdigit():
-            print("True")
-            ans_f = False
-            break
-        ans_f = True
-    if ans_f : 
-        print("False")
+#     ans_f = False
+#     for i in s:
+#         if i.isdigit():
+#             print("True")
+#             ans_f = False
+#             break
+#         ans_f = True
+#     if ans_f : 
+#         print("False")
         
-    ans_f = False
-    for i in s:
-        if i.islower():
-            print("True")
-            ans_f = False
-            break
-        ans_f = True
-    if ans_f : 
-        print("False")
+#     ans_f = False
+#     for i in s:
+#         if i.islower():
+#             print("True")
+#             ans_f = False
+#             break
+#         ans_f = True
+#     if ans_f : 
+#         print("False")
         
-    ans_f = False
-    for i in s:
-        if i.isupper():
-            print("True")
-            ans_f = False
-            break
-        ans_f = True
-    if ans_f : 
-        print("False")
+#     ans_f = False
+#     for i in s:
+#         if i.isupper():
+#             print("True")
+#             ans_f = False
+#             break
+#         ans_f = True
+#     if ans_f : 
+#         print("False")
             
+
+
