397a02f4e33f9d618ebf09eb818810112cc9a2ee
Nattapat Boonwirotrit <nattapat.boonwirotrit@gmail.com>
2025-07-30 20:49:47 +0700

    electric shop best buy

---
+++ Python/Electronics_Shop.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the getMoneySpent function below.
+#
+def getMoneySpent(keyboards, drives, b):
+    #
+    # Write your code here.
+    #
+    keyboards.sort()
+    drives.sort()
+    max_b = 0
+    print(keyboards, drives)
+    if keyboards[0] + drives[0] > b:
+        return -1
+    for k in keyboards :
+        for d in drives :
+            if k+d > b:
+                break
+            elif k+d > max_b:
+                max_b = k+d
+    return max_b
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    bnm = input().split()
+
+    b = int(bnm[0])
+
+    n = int(bnm[1])
+
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+
+    #
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    #
+
+    moneySpent = getMoneySpent(keyboards, drives, b)
+
+    fptr.write(str(moneySpent) + '\n')
+
+    fptr.close()
